4ec1a98fd8e377407e6fca02ebbe187ab5124d31
Kerben Bermingham <b.kerben2@gmail.com>
2019-03-25 22:57:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 function onClick(event) {
   console.log(event)
 }
+
 function onKeyDown(event) {
   console.log(event.keyCode)
 }
 
 const inputBox = document.getElementById('sup')
+
 function onInputKeyDown(event){
   console.log(event)
 }
